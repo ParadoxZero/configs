a4d536c624a52f396659fe3ffe14d7ce0b544a4f
Sidhin Thomas <thomas.sidhin@outlook.com>
2026-01-14 06:14:32 +0000

    Fix treesitter autocommand

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -4,10 +4,16 @@ return {
     lazy = false,
     build = ':TSUpdate',
 		config = function()
-      require'nvim-treesitter'.install { "lua", "python", "nu", "c", "cpp", "json", "bash", "gn", "idl" }
+      -- require('nvim-treesitter').install{ "lua", "python", "nu", "c", "cpp", "json", "bash", "gn", "idl" }
       vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'nu', 'json','gn','idl' },
-        callback = function() vim.treesitter.start() end,
+        pattern = { 'json','gn','idl', "lua", "nu" },
+        callback = function(event) 
+          local ft = vim.bo[event.buf].ft
+          local lang = vim.treesitter.language.get_lang(ft)
+          pcall(vim.treesitter.start, event.buf)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        end,
       })
 		end,
 	},
