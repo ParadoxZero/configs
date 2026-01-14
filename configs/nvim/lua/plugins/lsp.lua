0ab2dcd4aeade48f0c2074b03a966c18d8efcd94
Sidhin Thomas <thomas.sidhin@outlook.com>
2026-01-14 06:49:08 +0000

    Use blink instead of nvim-cmp

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
 	{
 		"mason-org/mason.nvim",
 		opts = {},
-    lazy = true,
+		lazy = true,
 		config = function()
 			require("mason").setup({
 				-- Optional configuration options
@@ -16,7 +16,7 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = { "williamboman/mason.nvim" },
-    lazy = true,
+		lazy = true,
 		opts = {
 			automatic_installation = true,
 		},
@@ -46,7 +46,7 @@ return {
 		},
 		config = function()
 			-- Setup clangd with Chromium defaults
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			capabilities.textDocument.foldingRange = {
 				dynamicRegistration = false,
 				lineFoldingOnly = true,
@@ -119,6 +119,80 @@ return {
 			})
 		end,
 	},
+	{
+		"saghen/blink.cmp",
+    version = '1.*',
+		opts = {
+			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+			-- 'super-tab' for mappings similar to vscode (tab to accept)
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			--
+			-- All presets have the following mappings:
+			-- C-space: Open menu or open docs if already open
+			-- C-n/C-p or Up/Down: Select next/previous item
+			-- C-e: Hide menu
+			-- C-k: Toggle signature help (if signature.enabled = true)
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = { preset = "super-tab" },
+
+			appearance = {
+				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
+
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+
+			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+			--
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+			completion = {
+				documentation = { auto_show = false },
+        menu = {
+          border = "rounded",
+        }
+			},
+		},
+		opts_extend = { "sources.default" },
+	},
+	-- Formatter
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "isort", "black", "autopep8" },
+					rust = { "rustfmt", lsp_format = "fallback" },
+					javascript = { "prettierd", "prettier", stop_after_first = true },
+					json = { "biome" },
+					jsonc = { "biome" },
+					cpp = { "clang-format", lsp_format = "fallback" },
+					go = { "gofmt" },
+				},
+			})
+			require("which-key").add({
+				{
+					"<Leader>cf",
+					function()
+						require("conform").format()
+					end,
+					{ noremap = true, silent = true },
+					desc = "Format Buffer",
+				},
+			})
+		end,
+	},
 	-- Autocompletion
 	{
 		"hrsh7th/nvim-cmp",
@@ -129,6 +203,7 @@ return {
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 		},
+		enabled = false,
 		config = function()
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
@@ -171,35 +246,6 @@ return {
 					{ name = "buffer" },
 					{ name = "path" },
 				}),
-			})
-		end,
-	},
-	-- Formatter
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					python = { "isort", "black", "autopep8" },
-					rust = { "rustfmt", lsp_format = "fallback" },
-					javascript = { "prettierd", "prettier", stop_after_first = true },
-					json = { "biome" },
-					jsonc = { "biome" },
-					cpp = { "clang-format", lsp_format = "fallback" },
-					go = { "gofmt" },
-				},
-			})
-			require("which-key").add({
-				{
-					"<Leader>cf",
-					function()
-						require("conform").format()
-					end,
-					{ noremap = true, silent = true },
-					desc = "Format Buffer",
-				},
 			})
 		end,
 	},
