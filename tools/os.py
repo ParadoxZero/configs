7ca73a9602ffb5988c8258a789aab48c98f47f1a
Sidhin S Thomas <sidhin.t@versa-networks.com>
2026-06-28 13:56:43 +0530

    Replace nmtui to impala

--- tools/os.py
+++ tools/os.py
@@ -18,6 +18,7 @@ class OS:
     def __init__(self, type: OSType, config_dir: Path) -> None:
         self.Type = type
         self.ConfigDir = config_dir
+        self.cargo_deps = [ "tree-sitter-cli", "zoxide", "nu", "bat", "yazi-build", "starship", "carapace-bin", "fnm", "uv",]
 
     def type(self):
         return self.Type
@@ -47,8 +48,7 @@ class OS:
         return self.ConfigDir / "starship.toml"
 
     def install_dependencies(self) -> bool:
-        deps = [ "tree-sitter-cli", "zoxide", "nu", "bat", "yazi-build", "starship", "carapace-bin", "fnm", "uv",]
-        for dep in deps:
+        for dep in self.cargo_deps:
             Run(f"cargo install {dep} --locked")
         return True
 
@@ -100,6 +100,7 @@ class Linux(OS):
     def __init__(self) -> None:
         config_dir = Path.home() / ".config"
         super().__init__(OSType.LINUX, config_dir)
+        self.cargo_deps += ['impala']
 
     def get_sway_path(self):
         return self.ConfigDir / "sway"
